5853b26ee0ba525c12051c4169f7f5445f1d8349
maheshwarieaga <maheshwari.eaga@gmail.com>
2026-01-06 15:01:56 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,7 +8,7 @@ def buildArtifact()
 }
 def deployTomcat(jobname,ip,context)
 {
-   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@{ip}:/var/lib/tomcat10/webapps/${context}.war"
+   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
 }
 def executeSelenium(jobname)
 {
